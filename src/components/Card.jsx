5f4aed9d2f50b9613f563744e858a00b8d740a47
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-05-06 18:22:53 +0200

    Change photo dimensions

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ function Card({ id, image, caption, onClick }) {
             onClick={() => onClick(id)}
             className="bg-white shadow rounded overflow-hidden cursor-pointer transform hover:scale-105 transition"
         >
-            <img src={image} alt={caption} className="w-full h-40 object-cover" />
+            <img src={image} alt={caption} className="w-full h-48 object-contain bg-white" />
             <p className="p-2 text-center text-sm text-gray-800 font-medium">
                 {caption}
             </p>
