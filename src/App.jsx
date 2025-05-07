bb523968240f148296c95f36262975a9f4221d55
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-05-07 11:42:59 +0200

    Add instruction message

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,9 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
       <h1 className="text-3xl font-bold text-blue-700 mb-6">🐶 Dog Breed Memory Game 🐶</h1>
+      <p className="text-gray-600 mb-6 text-center text-base sm:text-lg max-w-xl">
+      Get points by clicking on an image. Don't click on any more than once!
+      </p>
 
       <div className="flex gap-6 mb-6 text-lg font-semibold">
         <span>Score: {currentScore}</span>
